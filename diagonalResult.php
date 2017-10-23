f2378b5b72d2ee3f158ae113ef89e37410d6aad4
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-10-23 18:00:47 -0400

    - Fixed function inputValidate;

--- diagonalResult.php
+++ diagonalResult.php
@@ -2,17 +2,15 @@
 <?php
     require_once('functions/validateInput.php');
     require_once('functions/matrixCalculate.php');
-    
+
     $matrix = array(
         array($_POST['r0c0'], $_POST['r0c1'], $_POST['r0c2']),
         array($_POST['r1c0'], $_POST['r1c1'], $_POST['r1c2']),
         array($_POST['r2c0'], $_POST['r2c1'], $_POST['r2c2'])
     );
-    
+
     if(validadeInput($matrix)) 
     {
-        echo validadeInput($matrix);
-        
         $resultMain = mainDiagonalCalculate($matrix);
         $resultSecond = secondDiagonalCalculate($matrix);
     ?>
--- functions/validateInput.php
+++ functions/validateInput.php
@@ -2,19 +2,21 @@
     // Validate inputs
     function validadeInput($valueArray) 
     {
+        $status = true;
         foreach($valueArray as $index => $value) 
         {
             foreach($value as $k => $v) 
             {
-                return validade($v);
+                if (!isset($v) || ($v == null)) {
+                    $status = false;
+                    break;
+                }
+            }
+
+            if ($status == false) {
+                break;
             }
         }
+        return $status;
     }
-
-    function validade($value = null)
-    {
-        if (isset($value) && ($value == null)) {
-            return true;
-        }
-        return false;
-    }
+    
